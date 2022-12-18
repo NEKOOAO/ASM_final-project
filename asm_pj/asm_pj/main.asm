0a325905f6f6bab0a6d07ac5bd05db3c77331ed5
NEKOOAO <60414oscar@gmail.com>
2022-12-19 03:53:53 +0800

    read color PROC

--- asm_pj/asm_pj/main.asm
+++ asm_pj/asm_pj/main.asm
@@ -1,4 +1,5 @@
 INCLUDE Irvine32.inc
+INTLEN = 3
 .data
 filename byte  "a.txt"
 filehandle dword ?
@@ -14,12 +15,29 @@ convertINT PROC USES ecx ebx edx, arr: PTR byte , len : word
 LOOPINT:
 	mul ebx
 	mov dl,[esi]
-	sub dl,30h
+	sub dl,30h ; 30 -> 0's ascii
 	add eax,edx
 	inc esi
 	loop LOOPINT
 	ret
 convertINT ENDP
+
+convertCOLOR PROC USES ecx ebx edx, char:byte
+	movzx eax , char
+	cmp eax , 39h;9's ascii +1
+	JB ISINT
+	JMP COLOR
+ISINT:
+	sub eax, 30h ; 30h -> 0's ascii
+	JMP COLOR_RET
+COLOR:
+	sub eax, 41h ; 41h -> A's ascii
+	add eax, 10;
+	JMP COLOR_RET
+COLOR_RET:
+	ret
+convertCOLOR ENDP
+
 main PROC
 	INVOKE CreateFile, OFFSET filename, GENERIC_READ,DO_NOT_SHARE,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL, 0
 	mov filehandle , eax
@@ -27,8 +45,10 @@ main PROC
 	JNE L1
 	mov eax, 0
 L1:
-	INVOKE ReadFile,filehandle, offset input, 3, offset debug,0
-	INVOKE convertINT, addr input, 3
+	INVOKE ReadFile,filehandle, offset input, INTLEN, offset debug,0
+	INVOKE convertINT, addr input, INTLEN
+	INVOKE ReadFile,filehandle, offset input, 1, offset debug,0
+	INVOKE convertCOLOR, input[0]
 	exit
 main ENDP
 END main
